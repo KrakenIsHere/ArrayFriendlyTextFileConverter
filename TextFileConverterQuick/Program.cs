506a5f3c771f4404403445ca166650da2074f068
Kristian K. S. Nielsen <kris9463@edu.sde.dk>
2020-11-10 14:24:09 +0100

    Add project files.

---
+++ TextFileConverterQuick/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TextFileConverterQuick
+{
+    class Program
+    {
+        static bool isReverse = false;
+
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            var words = File.ReadAllLines(path + "/Words.txt");
+
+            List<string> vs = new List<string>();
+
+            if (!isReverse)
+            {
+                foreach (var str in words)
+                {
+                    if (!vs.Contains($"\"{str.ToLower()}\","))
+                    {
+                        vs.Add($"\"{str.ToLower()}\",");
+                    }
+                }
+
+                vs.Sort();
+            }
+            else
+            {
+                foreach (var str in words)
+                {
+                    var clean = str.ToLower().Replace("\"", "").Replace(",", "");
+
+                    clean.Trim();
+
+                    if (!vs.Contains($"{clean}"))
+                    {
+                        vs.Add($"{clean}");
+                    }
+                }
+
+                vs.Sort();
+            }
+            File.AppendAllLines(path + "/NewWords.txt", vs);
+        }
+    }
+}
